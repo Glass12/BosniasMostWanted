093956fd421f535f77a5a01072fbeafb07fba37e
Glass12 <64450770+Glass12@users.noreply.github.com>
2021-11-24 22:47:19 -0500

    Update Input.java

--- Input Greeting/Input.java
+++ Input Greeting/Input.java
@@ -4,9 +4,9 @@ public class Input {
   
   public static void main(String[] args) {
    
-    final Scanner xUsr = new Scanner(System.in);
+    Scanner xUsr = new Scanner(System.in);
     System.out.print("Enter name: ");
-    final String usr = xUser.nextLine();
+    String usr = xUser.nextLine();
     
     System.out.printf("Hello %s", usr);
     
