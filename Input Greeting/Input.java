02ff3d3eaff60d1f504fe97545e2ccc49b739b87
Glass12 <64450770+Glass12@users.noreply.github.com>
2021-11-24 22:00:34 -0500

    Create Input.java
    Java

---
+++ Input Greeting/Input.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Input {
+  
+  public static void main(String[] args) {
+   
+    Scanner xUsr = new Scanner(System.in);
+    System.out.print("Enter name: ");
+    String usr = xUser.nextLine();
+    
+    System.out.printf("Hello %s", usr);
+    
+  }
+}
