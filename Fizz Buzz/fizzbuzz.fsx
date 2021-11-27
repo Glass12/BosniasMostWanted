43c1d681a4d9949ab5618b0a9a4307275f0b4a63
Glass12 <64450770+Glass12@users.noreply.github.com>
2021-11-26 23:34:56 -0500

    Create fizzbuzz.fsx
    F#

---
+++ Fizz Buzz/fizzbuzz.fsx
@@ -0,0 +1,8 @@
+for n in 1 .. 100 do
+
+    let output =
+        [(3, "Fizz"); (5, "Buzz")] |>
+        List.map (fun (number, word) -> if n % number = 0 then word else "") |>
+        String.concat ""
+
+    if output = "" then printfn $"{n.ToString()} "else printfn $"{output}"
