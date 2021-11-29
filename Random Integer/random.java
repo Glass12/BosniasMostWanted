f89c37167f566b64e544dd90d9c6a7b3f1117751
Glass12 <64450770+Glass12@users.noreply.github.com>
2021-11-29 09:28:55 -0500

    Create random.java
    Java

---
+++ Random Integer/random.java
@@ -0,0 +1,10 @@
+public class random {
+  
+  public static void main(String[] args) {
+    
+    int randNum = (int) (Math.random() * ((10 - 1) + 1));
+    
+    System.out.printf("%d", randNum);
+    
+  }
+}
