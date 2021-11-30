e6132b0f2a0db21c3b9c1114d94da9cd71a2a7ee
Glass12 <64450770+Glass12@users.noreply.github.com>
2021-11-30 16:19:56 -0500

    Create fizzbuzz.v
    V

---
+++ Fizz Buzz/fizzbuzz.v
@@ -0,0 +1,13 @@
+fn main() {
+  
+  for i := 1; i < times + 1; i++ {
+
+	  mut fb := ""
+		
+	  if i % 3 == 0 { fb += "Fizz" }
+	  if i % 5 == 0 { fb += "Buzz" }
+      
+    if fb == "" {println("$i")} else {println(fb)}
+      
+    }      
+}
