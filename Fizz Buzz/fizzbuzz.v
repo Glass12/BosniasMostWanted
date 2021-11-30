8d7fb63aad00068568a00cf42c2495b58d682c5c
Glass12 <64450770+Glass12@users.noreply.github.com>
2021-11-30 16:20:23 -0500

    Update fizzbuzz.v
    V

--- Fizz Buzz/fizzbuzz.v
+++ Fizz Buzz/fizzbuzz.v
@@ -7,7 +7,7 @@ fn main() {
 	  if i % 3 == 0 { fb += "Fizz" }
 	  if i % 5 == 0 { fb += "Buzz" }
       
-    if fb == "" {println("$i")} else {println(fb)}
+    	  if fb == "" {println("$i")} else {println(fb)}
       
     }      
 }
