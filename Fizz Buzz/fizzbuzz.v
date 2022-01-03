ccdb7b775f6fc7eb4182dc45368881021da12e33
Semi-ProfessionalUsername <64450770+Semi-ProfessionalUsername@users.noreply.github.com>
2022-01-02 23:08:15 -0500

    Update fizzbuzz.v

--- Fizz Buzz/fizzbuzz.v
+++ Fizz Buzz/fizzbuzz.v
@@ -1,13 +1,13 @@
 fn main() {
   
-  for i := 1; i < times + 1; i++ {
+  for i := 1; i < 101; i++ {
 
 	  mut fb := ""
 		
 	  if i % 3 == 0 { fb += "Fizz" }
 	  if i % 5 == 0 { fb += "Buzz" }
       
-    	  if fb == "" {println("$i")} else {println(fb)}
+      if fb == "" {println("$i")} else {println(fb)}
       
     }      
 }
